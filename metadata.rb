0ed84a668f3c10f45b4a74a59de8c66c21756327
Thomas Massmann <thomas@propertyshelf.com>
2012-11-12 15:32:35 +0100

    Adjusted license information and cookbook description.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures zeoserver"
+maintainer       "Inqbus"
+maintainer_email "thomas.massmann@inqbus.de"
+license          "apache2"
+description      "Installs and configures a Zope ZEO-Server."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
